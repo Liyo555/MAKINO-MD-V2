77c0e21495f3e0a19a6686203ec74efd13bc2b05
Liyo555 <gamingliyo82@gmail.com>
2024-10-22 18:00:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.autoviewstatus = false;                 //make true to view statuses
 global.anticall = false
 
 
-global.SESSION_ID = process.env.SESSION_ID || ""
+global.SESSION_ID = process.env.SESSION_ID || "taira_tech-rply2Lxa#2FO0ejCVU_MD2XGPWUPewQmxD_JKIH6AEiu8Pnr7qyQ"
 global.Owner = ["2349151864541"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
